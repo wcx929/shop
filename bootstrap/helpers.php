b47206db6993c8f36323654d571c9776bdb34228
Wcx <1161993909@qq.com>
2020-02-15 12:15:49 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,10 +1,11 @@
 <?php
 
-/*自定义辅助函数*/
+	/*自定义辅助函数*/
 
 
 
-
-function test_helper() {
-    return 'OK';
-}
+	//此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制。在后面的章节中会用到。
+	function route_class()
+	{
+	    return str_replace('.', '-', Route::currentRouteName());
+	}
