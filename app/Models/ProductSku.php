d3c7047753acc5304155a11770a1e925db3c7417
Wcx <1161993909@qq.com>
2020-02-22 07:54:17 +0000

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use App\Exceptions\InternalException;
 
 
 class ProductSku extends Model
@@ -23,5 +24,22 @@ class ProductSku extends Model
             return $this->attributes['spec_img'];
         }
         return \Storage::disk('public')->url($this->attributes['spec_img']);
+    }
+
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
     }
 }
