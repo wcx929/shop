2dc5d514d132f1f778395b11cef5c2a7696296ee
Wcx <1161993909@qq.com>
2020-02-22 08:36:28 +0000

    用户界面 - 订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,9 +8,23 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
+use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku']) 
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+    
     public function store(OrderRequest $request)
     {
         $user  = $request->user();
@@ -66,7 +80,7 @@ class OrdersController extends Controller
 
             return $order;
         });
-
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
         return $order;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
     //提交订单
     Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
