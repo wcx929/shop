6bec73dfe6439a4e9282adbe7b1c267211401778
wanfeng <1161993909@qq.com>
2020-08-06 09:01:41 +0000

    修改es7type类型

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -81,7 +81,7 @@ class ProductsController extends Controller
         // 通过 whereIn 方法从数据库中读取商品数据
         $products = Product::query()->byIds($productIds)->get();
         // 返回一个 LengthAwarePaginator 对象
-        $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
+        $pager = new LengthAwarePaginator($products, $result['hits']['total']['value'], $perPage, $page, [
             'path' => route('products.index', false), // 手动构建分页的 url
         ]);
         return view('products.index', [
